e4a2803f6793ab368cdd3f2fa730a28771ee3e68
Mon <98190812+MonPanitan@users.noreply.github.com>
2022-12-09 10:48:59 +0000

    Commit

---
+++ Staff/Class.java
@@ -0,0 +1,8 @@
+package Staff;
+
+public enum Class {
+
+JUNIOR_CLASS,
+SENIOR_CLASS
+
+}
---
+++ Staff/Trainer.java
@@ -0,0 +1,12 @@
+package Staff;
+
+public class Trainer extends MemberStaff {
+    private Class classAssign;
+
+        @Override
+        public String toString() {
+            return "Class []";
+        };
+
+
+}
